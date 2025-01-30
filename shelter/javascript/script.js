060acfd7d7bcc09ef6c2bf2d9c95520cdd1e576c
Anastasia Bank <signofaries@gmail.com>
2025-01-30 10:09:12 +0300

    feat: add nav list for burger menu

---
+++ shelter/javascript/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = this.getElementById('burgerMenu');
+    const menu = this.querySelector('.navigation__list');
+    console.log(menu);
+});
+
+console.log(burgerMenu);
+
+
+console.log(menu);
