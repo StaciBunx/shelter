2d0d17b46b3997991c9e68a0b8695a4feea4a3a5
Anastasia Bank <signofaries@gmail.com>
2025-01-30 16:20:29 +0300

    feat: add overlay under dropdown menu

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = this.getElementById('burgerMenu');
     const menu = this.querySelector('.navigation__list');
-    // const pageBody = this.querySelector('page__body');
+    const overlay = this.getElementById('overlay');
     burgerMenu.addEventListener('click', function () {
         burgerMenu.classList.toggle('burger_opened');
         menu.classList.toggle('navigation__list_opened');
+        overlay.classList.toggle('overlay_active');
     })
+
+    overlay.addEventListener('click', function () {
+        burgerMenu.classList.remove('burger_opened');
+        menu.classList.remove('navigation__list_opened');
+        overlay.classList.remove('overlay_active');
+    });
+
 });
