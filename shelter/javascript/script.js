8dbb1f62ff65fda359d8c7afd06108d8638cffe7
Anastasia Bank <signofaries@gmail.com>
2025-01-30 17:03:20 +0300

    fix: opened burger-menu in light color

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -2,10 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = this.getElementById('burgerMenu');
     const menu = this.querySelector('.navigation__list');
     const overlay = this.getElementById('overlay');
+    // const burgerLine = this.querySelectorAll('burger-line');
+
     burgerMenu.addEventListener('click', function () {
         burgerMenu.classList.toggle('burger_opened');
         menu.classList.toggle('navigation__list_opened');
         overlay.classList.toggle('overlay_active');
+
+        // // For catalog page
+        // // burgerLine.classList.toggle('burger-line_dark');
+        // console.log(burgerLine);
     })
 
     overlay.addEventListener('click', function () {
