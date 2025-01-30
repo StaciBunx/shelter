89a28524be536a1a9d7e922c05356c07d20db1e8
Anastasia Bank <signofaries@gmail.com>
2025-01-30 11:08:08 +0300

    feat: add animation to burger-menu by click

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = this.getElementById('burgerMenu');
     const menu = this.querySelector('.navigation__list');
-    console.log(menu);
+    burgerMenu.addEventListener('click', function () {
+        burgerMenu.classList.toggle('burger_opened');
+    })
 });
-
-console.log(burgerMenu);
-
-
-console.log(menu);
