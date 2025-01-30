8b437ea4b5b343bd73db329011f296096c03681f
Anastasia Bank <signofaries@gmail.com>
2025-01-30 12:48:26 +0300

    fix: remove horizontal scroll with burger menu for medium screens

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = this.getElementById('burgerMenu');
     const menu = this.querySelector('.navigation__list');
+    // const pageBody = this.querySelector('page__body');
     burgerMenu.addEventListener('click', function () {
         burgerMenu.classList.toggle('burger_opened');
         menu.classList.toggle('navigation__list_opened');
