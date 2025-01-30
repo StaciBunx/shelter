df8e54e109f26d0777f98c41b3b91ec16e9285d0
Anastasia Bank <signofaries@gmail.com>
2025-01-30 11:10:58 +0300

    feat: add animation to nav list by click

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -3,5 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const menu = this.querySelector('.navigation__list');
     burgerMenu.addEventListener('click', function () {
         burgerMenu.classList.toggle('burger_opened');
+        menu.classList.toggle('navigation__list_opened');
     })
 });
