0395ca706dfeb0b7999841ddfd0264463e4ae276
Anastasia Bank <signofaries@gmail.com>
2025-01-31 11:03:56 +0300

    feat: add no-scroll while open burger-menu

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const menu = this.querySelector('.navigation__list');
     const overlay = this.getElementById('overlay');
     const links = this.querySelectorAll('.navigation__link');
+    const body = this.body;
 
     // Open/close menu by click on burger
     burgerMenu.addEventListener('click', function () {
         burgerMenu.classList.toggle('burger_opened');
         menu.classList.toggle('navigation__list_opened');
         overlay.classList.toggle('overlay_active');
+        body.classList.toggle('no-scroll');
     })
 
     // Open/close menu and overlay by click on overlay
@@ -16,6 +18,8 @@ document.addEventListener('DOMContentLoaded', function () {
         burgerMenu.classList.remove('burger_opened');
         menu.classList.remove('navigation__list_opened');
         overlay.classList.remove('overlay_active');
+        body.classList.remove('no-scroll');
+
     });
 
     // Open/close menu and overlay by click on link in the menu
@@ -24,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
             burgerMenu.classList.remove('burger_opened');
             menu.classList.remove('navigation__list_opened');
             overlay.classList.remove('overlay_active');
+            body.classList.remove('no-scroll');
         });
 
     });
