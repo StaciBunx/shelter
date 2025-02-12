09668e5e1dc1cf824d5a808323d4a9c914c5a73e
Anastasia Bank <signofaries@gmail.com>
2025-02-12 18:14:24 +0300

    docs: start create js modules

---
+++ shelter/javascript/modules/common.js
@@ -0,0 +1,8 @@
+export function initBurgerMenu () {
+    const burgerMenu = document.querySelector(SELECTORS.BURGER_MENU);
+    const menu = document.querySelector(SELECTORS.MENU);
+    const overlay = document.querySelector(SELECTORS.OVERLAY);
+    const body = document.querySelector(SELECTORS.BODY);
+
+
+}
