3656cd26ba71cf1f10084125bf97ffe44f4b22b6
Anastasia Bank <signofaries@gmail.com>
2025-02-13 12:12:26 +0300

    docs: add fetchPetsData to common modules

--- shelter/javascript/modules/common.js
+++ shelter/javascript/modules/common.js
@@ -33,3 +33,17 @@ export function initBurgerMenu () {
         });
     }
 }
+
+// Fetch data from JSON
+export async function fetchPetsData () {
+    try {
+        const response = await fetch('./data/pets.json');
+        if (!response.ok) {
+            throw new Error('Ошибка загрузки данных c pets JSON');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Ошибка: ', error);
+    }
+}
