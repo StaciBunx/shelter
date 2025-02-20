f167cd27d54b2a535ff16563de540cb41ab23d99
Anastasia Bank <signofaries@gmail.com>
2025-02-20 17:49:32 +0300

    docs: create modal window module

---
+++ shelter/javascript/modules/modal.js
@@ -0,0 +1,11 @@
+export function initModalWindow () {
+    const modalWindow = document.querySelector('modal');
+    const modalWindowCloseBtn = document.querySelector('modal-close');
+
+    if (modalWindow && modalWindowCloseBtn) {
+        modalWindowCloseBtn.addEventListener('click', function () {
+            modalWindow.classList.remove('modal_opened');
+        });
+
+    }
+}
--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -1,9 +1,10 @@
 import { initBurgerMenu } from './modules/burger.js';
 import { initCarousel } from './modules/home.js';
 import { initCatalog } from './modules/catalog.js';
-
+import { initModalWindow } from './modules/modal.js';
 document.addEventListener('DOMContentLoaded', function () {
     initBurgerMenu();
+    initModalWindow();
 
     if (document.querySelector('.home-page')) {
         initCarousel();
