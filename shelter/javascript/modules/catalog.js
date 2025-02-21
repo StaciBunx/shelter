0650cd520b95bcab6eea7e80f8bd30df1a11c43e
Anastasia Bank <signofaries@gmail.com>
2025-02-21 14:24:27 +0300

    docs: rename common --> pet-cards

--- shelter/javascript/modules/catalog.js
+++ shelter/javascript/modules/catalog.js
@@ -1,4 +1,4 @@
-import { fetchPetsData, shuffleArray, renderPetCard } from '../modules/common.js';
+import { fetchPetsData, shuffleArray, renderPetCard } from './pet-cards.js';
 
 // DOM elements
 const catalogContainer = document.querySelector('.pets__cards');
