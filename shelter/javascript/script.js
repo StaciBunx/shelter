82b56dba9b578e1edae58ea0546a8149e81ac6af
Anastasia Bank <signofaries@gmail.com>
2025-02-21 14:22:58 +0300

    feat: open/close modal window

--- shelter/javascript/script.js
+++ shelter/javascript/script.js
@@ -2,15 +2,18 @@ import { initBurgerMenu } from './modules/burger.js';
 import { initCarousel } from './modules/home.js';
 import { initCatalog } from './modules/catalog.js';
 import { initModalWindow } from './modules/modal.js';
+
 document.addEventListener('DOMContentLoaded', function () {
     initBurgerMenu();
-    initModalWindow();
 
     if (document.querySelector('.home-page')) {
-        initCarousel();
+        initCarousel().then(() => {
+            initModalWindow();
+        });
     }
 
     if (document.querySelector('.catalog-page')) {
         initCatalog();
     }
+
 });
