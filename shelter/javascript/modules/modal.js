01c3acb97ebbdbe9f2931a4030d24d8ddd843e46
Anastasia Bank <signofaries@gmail.com>
2025-04-10 17:06:30 +0300

    feat: fix issues with scroll width while modal is opened

--- shelter/javascript/modules/modal.js
+++ shelter/javascript/modules/modal.js
@@ -4,13 +4,35 @@ const cardsContainer = document.querySelector('.cards-container');
 const overlay = document.querySelector('#overlay');
 const body = document.querySelector('.page__body');
 
+
+function getScrollbarWidth () {
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll';
+    body.appendChild(outer);
+
+    const inner = document.createElement('div');
+    outer.appendChild(inner);
+
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+    outer.parentNode.removeChild(outer);
+    return scrollbarWidth;
+}
+
 function openModal () {
+    const scrollbarWidth = getScrollbarWidth();
+    document.body.style.overflow = 'hidden';
+    document.body.style.paddingRight = `${scrollbarWidth}px`;
+
     modalWindow.classList.add('modal_opened');
     overlay.classList.toggle('overlay_active');
     body.classList.toggle('no-scroll');
 }
 
 function closeModal () {
+    document.body.style.overflow = '';
+    document.body.style.paddingRight = '';
+
     modalWindow.classList.remove('modal_opened');
     overlay.classList.remove('overlay_active');
     body.classList.remove('no-scroll');
