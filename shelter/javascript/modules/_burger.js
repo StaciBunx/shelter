439036601b4a80a8b20023625b2375f4d98f9aa9
Anastasia Bank <signofaries@gmail.com>
2025-04-25 14:30:07 +0300

    refactor: update _burger

--- shelter/javascript/modules/_burger.js
+++ shelter/javascript/modules/_burger.js
@@ -1,25 +1,24 @@
+import { showOverlay, hideOverlay } from "./_overlay";
+
 // Burger-menu control
 export function initBurgerMenu () {
     const burgerMenu = document.querySelector('#burgerMenu');
     const menu = document.querySelector('.navigation__list');
-    const overlay = document.querySelector('#overlay');
     const body = document.querySelector('.page__body');
 
-    if (burgerMenu && menu && overlay && body) {
+    if (burgerMenu && menu && body) {
         // Open/close by click on icon
         burgerMenu.addEventListener('click', function () {
             burgerMenu.classList.toggle('burger_opened');
             menu.classList.toggle('navigation__list_opened');
-            overlay.classList.toggle('overlay_active');
-            body.classList.toggle('no-scroll');
+            showOverlay();
         });
 
         // Close menu by click on overlay
         overlay.addEventListener('click', function () {
             burgerMenu.classList.remove('burger_opened');
             menu.classList.remove('navigation__list_opened');
-            overlay.classList.remove('overlay_active');
-            body.classList.remove('no-scroll');
+            hideOverlay();
         });
 
         // Close menu by click on menu link
@@ -27,8 +26,7 @@ export function initBurgerMenu () {
             if (event.target.classList.contains('navigation__link')) {
                 burgerMenu.classList.remove('burger_opened');
                 menu.classList.remove('navigation__list_opened');
-                overlay.classList.remove('overlay_active');
-                body.classList.remove('no-scroll');
+                hideOverlay();
             }
         });
     }
