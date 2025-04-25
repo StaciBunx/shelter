3dfdf2b5425b2545c8f9654160ed97b0aad7d9c4
Anastasia Bank <signofaries@gmail.com>
2025-04-25 14:08:57 +0300

    refactor: move overlay functions to separate module

--- shelter/javascript/modules/_modal.js
+++ shelter/javascript/modules/_modal.js
@@ -1,43 +1,19 @@
+import { showOverlay, hideOverlay } from "./_overlay";
+
 // DOM elements
 const modalWindow = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal-container');
 const modalWindowCloseBtn = document.querySelector('.modal-close');
 const cardsContainer = document.querySelector('.cards-container');
-const overlay = document.querySelector('#overlay');
-const body = document.querySelector('.page__body');
-
-
-function getScrollbarWidth () {
-    const outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.overflow = 'scroll';
-    body.appendChild(outer);
-
-    const inner = document.createElement('div');
-    outer.appendChild(inner);
-
-    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
-    outer.parentNode.removeChild(outer);
-    return scrollbarWidth;
-}
 
 function openModal () {
-    const scrollbarWidth = getScrollbarWidth();
-    document.body.style.overflow = 'hidden';
-    document.body.style.paddingRight = `${scrollbarWidth}px`;
-
     modalWindow.classList.add('modal_opened');
-    overlay.classList.add('overlay_active');
-    body.classList.add('no-scroll');
+    showOverlay();
 }
 
 function closeModal () {
-    document.body.style.overflow = '';
-    document.body.style.paddingRight = '';
-
     modalWindow.classList.remove('modal_opened');
-    overlay.classList.remove('overlay_active');
-    body.classList.remove('no-scroll');
+    hideOverlay();
 }
 
 export function initModalWindow () {
@@ -59,6 +35,6 @@ export function initModalWindow () {
     }
 
     else {
-        console.error('Один из элементов не найден в DOM:', { modalWindow, modalWindowCloseBtn, cardsContainer, overlay, modalContainer });
+        console.error('Один из элементов не найден в DOM:', { modalWindow, modalWindowCloseBtn, cardsContainer, modalContainer });
     }
 }
